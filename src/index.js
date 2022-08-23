30505236c337a3579ace40140e4d599a0cd2ca20
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-24 01:38:20 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleTask(e.target.newTask.value);
+  });
+
+  function handleTask(task) {
+    let li = document.createElement("li");
+    let del = document.createElement("button");
+    li.innerHTML = task;
+    document.querySelector("ul").appendChild(li);
+    document.querySelector("li").appendChild(del);
+    document.querySelector("button").addEventListener("click", () => {
+      document.querySelector("li").remove();
+    });
+  }
 });
